bec7aced9fb3553c4541d96d027a862d1fed4ca7
Jeroen <jeroen@joche.ms>
2019-06-13 17:09:49 +0200

    Adds event for event tracking

--- src/Jobs/DispatchEvent.php
+++ src/Jobs/DispatchEvent.php
@@ -1,11 +1,17 @@
 <?php namespace TemperWorks\LaravelOptimizely\Jobs;
 
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Optimizely\Event\Dispatcher\DefaultEventDispatcher;
 use Optimizely\Event\LogEvent;
 
 class DispatchEvent implements ShouldQueue
 {
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
     public $event;
 
     public function __construct(LogEvent $event)
---
+++ src/Jobs/TrackEvent.php
@@ -0,0 +1,31 @@
+<?php namespace TemperWorks\LaravelOptimizely\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Optimizely;
+
+class TrackEvent implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $event;
+    protected $userID;
+    protected $params;
+
+    const APPLIED_TO_SHIFTS = "applied to shifts";
+
+    public function __construct($event, $userID, $params=[])
+    {
+        $this->event = $event;
+        $this->userID = $userID;
+        $this->params = $params;
+    }
+
+    public function handle()
+    {
+        Optimizely::track($event, $userID, $params);
+    }
+}
